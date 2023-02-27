50e35ceb7af0282dafdcf6e5ef002a794ea77058
Jamie Miles <jamie.miles@weston.ac.uk>
2023-02-27 13:53:36 +0000

    Created the student class ready for the main function

--- Python_Bootcamp_Week_8/Python_Bootcamp_Week_8.py
+++ Python_Bootcamp_Week_8/Python_Bootcamp_Week_8.py
@@ -2,15 +2,16 @@ class MenuOutOfRange(Exception):
     #This class is for triggering a custom exception
     pass
 
-class Students():
+class Students(): #Class template for student objects
 
+    #These are the member attributes/variables to define our object
     firstName = ""
     secondName = ""
     age = 0
     className = ""
     Attendance = False
 
-    def __init__(self, firstName, secondName, age):
+    def __init__(self, firstName, secondName, age): #Constructor used to initialize our student objects
 
         self.firstName = firstName
         self.secondName = secondName
