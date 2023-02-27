864971f2c466fff7b8f17fe08c2d6f4ec9ea7e50
Jamie Miles <jamie.miles@weston.ac.uk>
2023-02-27 11:27:45 +0000

    Started try block validation inside the RunMenu function

--- Python_Bootcamp_Week_8/Python_Bootcamp_Week_8.py
+++ Python_Bootcamp_Week_8/Python_Bootcamp_Week_8.py
@@ -1,14 +1,26 @@
 def RunMenu(menuList):
 
-    #Start text to the screen with the initial value in the passed list
-    print(menuList[0])
+    continueLoop = True
+    while continueLoop == True:
 
-    for x in range(1, len(menuList)): #Loops from 1 (because be have used index 0 already), until the end of the indexes in our passed list
-        print(x + ": " + menuList[x]) #Prints out a formatted text screen, using the text passed up to the function inside the list, using x as the index counter
+        try: #Tells the compiler to attempt the code indented, passing to an exception if an error occurs
+            
+            #Start text to the screen with the initial value in the passed list
+            print(menuList[0])
 
-    menuChoice = int(input("\nPlease make a selection from the list provided:\n")) #Requests the user enter an int - then saves it into the variable assignment
+            for x in range(1, len(menuList)): #Loops from 1 (because be have used index 0 already), until the end of the indexes in our passed list
+                print(str(x) + ": " + menuList[x]) #Prints out a formatted text screen, using the text passed up to the function inside the list, using x as the index counter
 
-    return menuChoice #Returns the user's input
+            menuChoice = int(input("\nPlease make a selection from the list provided:\n")) #Requests the user enter an int - then saves it into the variable assignment
+
+            return menuChoice #Returns the user's input
+
+        except: #Triggers exception - no specific condition required.
+
+            print("\n\nError! An unknown bug as occurred, please try again...")
+            input("[Press Enter to Try Again]\n\n")
+
+        
 
 continueLoop = True #Sets my loop condition variable to True
 while continueLoop == True: #Will loop this section of code until the variable condition is changed to false
