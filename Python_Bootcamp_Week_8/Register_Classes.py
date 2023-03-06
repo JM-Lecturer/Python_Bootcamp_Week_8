bcefe8f1e14ac7f05a67364aac5d96f47c5ac2cf
Jamie Miles <jamie.miles@weston.ac.uk>
2023-03-06 13:47:38 +0000

    Created a new module for my programs classes

---
+++ Python_Bootcamp_Week_8/Register_Classes.py
@@ -0,0 +1,33 @@
+class ValueOutOfRange(Exception):
+    #This class is for triggering a custom exception
+    pass
+
+class Students(): #Class template for student objects
+
+    #These are the member attributes/variables to define our object
+    firstName = ""
+    secondName = ""
+    age = 0
+    Attendance = False
+
+    def __init__(self, firstName, secondName, age): #Constructor used to initialize our student objects
+
+        self.firstName = firstName
+        self.secondName = secondName
+        self.age = age
+
+    def GetName(self): #Member function, simply returns a concat string of the students name
+
+        return self.firstName + " " + self.secondName
+
+class Classes():
+    
+    className = ""
+    classStudents = list()
+
+    def __init__(self, className):
+
+        self.className = className
+
+
+
